e71585f8fa33ca480f54ce240516cf1a620e482f
Nick Kelly <importantnk@gmail.com>
2017-07-31 15:50:24 -0400

    == False to is False

--- updatescript.py
+++ updatescript.py
@@ -6,7 +6,7 @@ This script will be used to autpo update the pistation2
 it will also cause the blue ledd to light up until the update is
 over.
 
-this makes systmem calls to apt-get in order to update the machine
+this makes system calls to apt-get in order to update the machine
 in every way I could think.
 """
 
@@ -21,12 +21,11 @@ GPIO.setup(21, GPIO.OUT)
 
 while True:
     input_state = GPIO.input(26)
-    if input_state == False:
+    if input_state is False:
         GPIO.output(21, GPIO.HIGH)
         os.system("sudo apt-get update")
         os.system("sudo apt-get -y upgrade")
         os.system("sudo apt-get -y dist-upgrade")
         os.system("sudo apt-get -y install rpi-update")
         GPIO.output(21, GPIO.LOW)
-        print('Done.')
     time.sleep(.2)
