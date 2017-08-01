1753fa0bea21a2edf2911cb106c9e9f1edfa2f6e
Nick Kelly <importantnk@gmail.com>
2017-08-01 13:50:35 -0400

    cleaned up code.

--- buttontest.py
+++ buttontest.py
@@ -3,13 +3,12 @@ Nicholas Kelly
 Button test
 
 This program will simply print a statement if an input is detected on a
-gpio pin. for the pursposes we are currently using this will involve the
+gpio pin. for the purposes we are currently using this will involve the
 GPIO 04 pin
 """
 
 import RPi.GPIO as GPIO
 import time
-import os
 
 GPIO.setmode(GPIO.BCM)
 
@@ -17,6 +16,6 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(4)
-    if input_state == False:
-        os.system('shutdown -r now')
+    if input_state is False:
+        print('A button has been pressed.')
     time.sleep(.2)
